1547cd7aec0987f0c9e6eee7a499965093040b52
josdandar <josdandar@gmail.com>
2024-11-13 00:08:02 -0800

    Intro Next.js 15

---
+++ app/(root)/about/page.tsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+const Page = () => {
+    return (
+        <div>About</div>
+    )
+}
+
+export default Page //En next las rutas son los folders y la page.tsx es lo que se va a renderizar : )
+//
